6240f4799498e462f18dad36429306672e512bf6
JeRee69 <128933390+JeRee69@users.noreply.github.com>
2025-06-17 12:59:11 -0400

    Coreccion error

--- visita-facil-api/src/main/java/com/construsoft/visita_facil_api/controller/AccountController.java
+++ visita-facil-api/src/main/java/com/construsoft/visita_facil_api/controller/AccountController.java
@@ -1,16 +1,21 @@
 package com.construsoft.visita_facil_api.controller;
 
-import com.construsoft.visita_facil_api.domain.AccountProfileDTO;
-import com.construsoft.visita_facil_api.model.Account;
-import com.construsoft.visita_facil_api.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.construsoft.visita_facil_api.domain.AccountProfileDTO;
+import com.construsoft.visita_facil_api.model.Account;
+import com.construsoft.visita_facil_api.service.AccountService;
 
 @RestController
 @RequestMapping("/account")
@@ -39,7 +44,6 @@ public class AccountController {
         if (accountOpt.isPresent()) {
             Map<String, Long> response = new HashMap<>();
             response.put("accountId", accountOpt.get().getId());
-            response.put("correo", accountOpt.get().getEmail()); // Agregamos el correo aquí
             return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
